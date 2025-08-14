9e2d48215e54d29e995f4f149c7a112c4fc755a7
kebsolarte <kcvolarte@gmail.com>
2025-08-15 03:04:53 +0800

    added new functions for simplicity, added flip aces functionality, fixed game loop

--- udemy_100days_angela/blackjack/blackjack.py
+++ udemy_100days_angela/blackjack/blackjack.py
@@ -1,4 +1,5 @@
 import random 
+import os
 
 # simple list of cards in the deck (A as 11; J, Q, K as 10)
 deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,]
@@ -8,11 +9,14 @@ def deal_hand(deck, num):
     cards = random.choices(population=deck, k=num)
     return cards
 
+def additional_draw(hand, deck):
+    new_card = deal_hand(deck, 1)
+    hand.extend(new_card)
+    return hand
+
 def sum_cards(cards):
-    total = 0
-    for card in cards:
-        total += card
-    return total
+    # sum() function can take an iterable as an input
+    return sum(cards)
 
 def blackjack_difference(cards):
     difference = 21 - sum_cards(cards)
@@ -23,16 +27,35 @@ def over_21(cards):
         return True
     else: 
         return False
+    
+def flip_aces(cards):
+    while over_21(cards) and 11 in cards:
+        cards.remove(11)
+        cards.append(1)
+    return cards
+
+def determine_winner(dealer_score, player_score):
+    if dealer_score < player_score:
+        result = "The dealer wins!"
+    elif dealer_score == player_score:
+        result = "It's a draw!"
+    else:
+        result = "The player wins!"
+    return result
+
+
 
 
 # main program
 game_over = False
-dealer = []
-player = []
-
-print("Welcome to the simple blackjack game!\n")
 
 while not game_over:
+    print("Welcome to the simple blackjack game!\n")
+
+    # initialize empty hands
+    dealer = []
+    player = []
+
     # initial draw for dealer and player
     dealer = deal_hand(deck, 2)
     player = deal_hand(deck, 2)
@@ -44,29 +67,31 @@ while not game_over:
     # asks player if player wants another card
     choice = input("Would you like another card? (Y/N): ").lower()
 
+    # used extend since the output of deal_hand function is a list
     if choice == 'y':
-        additional_draw = deal_hand(deck, 1)
-        player.extend(additional_draw)
-        print(f"Your cards are {player}.\n")
+        additional_draw(player, deck)
 
     # draw another card for dealer if total is below 17
     if sum_cards(dealer) < 17:
-        additional_draw = deal_hand(deck, 1)
-        dealer.extend(additional_draw)
-        print("\nThe dealer also asked for another card.\n")
+        additional_draw(dealer, deck)
+        print("\nThe dealer asked for another card.\n")
+
+    # flips aces when necessary before checking the overall winner
+    flip_aces(dealer)
+    flip_aces(player)
 
     # disqualify dealer or player if sum is over 21
     if over_21(dealer) and not over_21(player):
         print("The dealer's hand is over 21, player wins!")
-        break
+        game_over = True
 
     elif over_21(player) and not over_21(dealer):
         print("The player's hand is over 21, dealer wins!")
-        break
+        game_over = True
 
     elif over_21(dealer) and over_21(player):
         print("Both hands are over 21, no one wins.")
-        break 
+        game_over = True 
 
     # valid game after extra draws
     print(f"\nThe dealer's cards are {dealer}")
@@ -74,17 +99,20 @@ while not game_over:
     print(f"The dealer's total is {sum_cards(dealer)}, while the player's total is {sum_cards(player)}.")
 
     # computes the blackjack differences
-    dealer_hand = blackjack_difference(dealer)
-    player_hand = blackjack_difference(player)
+    dealer_score = blackjack_difference(dealer)
+    player_score = blackjack_difference(player)
 
     # determine the winner
-    if dealer_hand < player_hand:
-        print("The dealer wins!")
-    elif dealer_hand == player_hand:
-        print("It's a draw!")
-    else:
-        print("The player wins!")
+    result = determine_winner(dealer_score, player_score)
+    print(result)
 
-    game_over = True
+    # ask if player wants to keep playing
+    new_game = input("Do you want to keep playing? (Y/N): ").lower()
+
+    if new_game == 'y':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        game_over = False
+    else:
+        game_over = True
         
     
