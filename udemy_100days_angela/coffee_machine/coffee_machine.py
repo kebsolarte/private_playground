ecace9b9bb0cd64d5b3b877f9426df59a340dd83
kebsolarte <kcvolarte@gmail.com>
2025-08-29 08:23:52 +0800

    Added functions and main program to coffee machine

--- udemy_100days_angela/coffee_machine/coffee_machine.py
+++ udemy_100days_angela/coffee_machine/coffee_machine.py
@@ -1,3 +1,4 @@
+# available coffee types and their attributes
 COFFEE_TYPES = {
     "espresso": {
         "ingredients": {
@@ -25,12 +26,15 @@ COFFEE_TYPES = {
     },
 }
 
+
+# resources bank at full capacity
 RESOURCES_FULL = {
     "water": 300,
     "coffee": 100,
     "milk": 200,
 }
 
+# coin values in cents
 COIN_VALUES = {
     "penny": 0.01,
     "nickel": 0.05,
@@ -38,18 +42,31 @@ COIN_VALUES = {
     "quarter": 0.25,
 }
 
-def report_resources():
+
+def report_resources(resource_bank):
+    """Reports current level of resources."""
     print(f"""
           The remaining resources are:
-          Water: {RESOURCES_FULL["water"]} mL
-          Milk: {RESOURCES_FULL["milk"]} mL
-          Coffee: {RESOURCES_FULL["coffee"]} g
+          Water: {resource_bank["water"]} mL
+          Milk: {resource_bank["milk"]} mL
+          Coffee: {resource_bank["coffee"]} g
           """)
     
+def update_resources(resource_bank, coffee_type):
+    """Updates resource bank after every successful order."""
+    water = resource_bank["water"] - COFFEE_TYPES[coffee_type]["ingredients"]["water"]
+    coffee = resource_bank["coffee"] - COFFEE_TYPES[coffee_type]["ingredients"]["coffee"]
+    milk =resource_bank["milk"] - COFFEE_TYPES[coffee_type]["ingredients"]["milk"]
+
+    return {"water": water, "coffee": coffee, "milk": milk}
+
 def refill_resources():
+    """Resets the level of resources."""
     return RESOURCES_FULL
 
+
 def check_resources(resource_bank, coffee_type):
+    """Checks if resources are enough for the desired coffee type."""
     if all(
         resource_bank["water"] >= COFFEE_TYPES[coffee_type]["ingredients"]["water"],
         resource_bank["coffee"] >= COFFEE_TYPES[coffee_type]["ingredients"]["coffee"],
@@ -58,8 +75,28 @@ def check_resources(resource_bank, coffee_type):
         return True
     else:
         return False
+    
+def check_availability(order, resource_bank):
+    """Checks if order is available."""
+    if order not in [coffee for coffee in COFFEE_TYPES.keys]:
+        print(f"Sorry {order} is not available. Please try again.")
+        return False
+    elif not check_resources(resource_bank, order):
+        print(f"Sorry {order} is not available. We need to refill our stocks for that. Apologies. Please select another option.")
+        return False
+    else:
+        return True
+        
 
-def sum_payment(penny, nickel, dime, quarter):
+def get_payment():
+    """Computes for the total value of coins inserted."""
+    # asks for coin counts
+    penny = int(input("Pennies: "))
+    nickel = int(input("Nickels: "))
+    dime = int(input("Dimes: "))
+    quarter = int(input("Quarters: "))
+
+    # computes for the total payment value
     penny = penny * COIN_VALUES["penny"]
     nickel = nickel * COIN_VALUES["nickel"]
     dime = dime * COIN_VALUES["dime"]
@@ -70,21 +107,75 @@ def sum_payment(penny, nickel, dime, quarter):
     return total
 
 def check_payment(payment, coffee_type):
+    """Checks if payment is enough for the order."""
     if payment >= COFFEE_TYPES[coffee_type]["price"]:
         return True
     else:
         return False
 
 def give_change(payment, coffee_type):
+    """Computes and returns the change to the customer, if any."""
     change = payment - COFFEE_TYPES[coffee_type]["price"]
-    return change
+    
+    if change == 0:
+        print("Thank you for giving the exact amount!")
+    else:
+        print(f"Thank you. Here's your change of ${change}.")
+
+def another_transaction():
+    """Asks if user wants another transaction."""
+    repeat = input("Would you like another transaction (y/n)?: ").lower()
+
+    if repeat == 'y':
+        return True
+    else: 
+        return False
 
 
-# TODO: create coin operation function
+# main program
+successful_transaction = False
 
-# TODO: ask for the coins and gives changes
+resource_bank = {
+    "water": 300,
+    "coffee": 100,
+    "milk": 200,
+}
 
-# TODO: check success of transaction
+while not successful_transaction:
+    print("Welcome to Kevin's coffee machine!")
 
+    # take order
+    order = input("What do you want today? I can make you an espresso, latte, or cappuccino!: ").lower()
 
+    # secret maintenance functions for operators
+    if order == 'report':
+        report_resources(resource_bank)
+    elif order == 'refill':
+        resource_bank = refill_resources()
 
+    # check availability
+    elif not check_availability(order, resource_bank): continue
+
+    # proceeds with order and asks for payment
+    else:
+        print(f"Nice order! That would be ${COFFEE_TYPES[order]["price"]} only. Kindly insert your coins below:")
+        payment = get_payment()
+
+        # checks if payment is enough, and proceeds with making order if it is
+        if check_payment(payment, order):
+            give_change(payment, order)
+            print(f"Let me make your {order}.")
+            print("Creating yummy magic ...")
+            print("Dispensing ...")
+            print(f"Thank you for waiting! Here's you {order}. Have a nice day!")
+            resource_bank = update_resources(resource_bank, order)
+        else:
+            print("Sorry, you have entered an insufficient amount.")
+            print(f"Giving you back your ${payment}. Please try again.")
+
+    if another_transaction():
+        continue
+    else:
+        successful_transaction = True
+
+    
