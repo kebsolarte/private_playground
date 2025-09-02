f763c581e9bf53e80dc4c592e3ca0a7acba60af6
kebsolarte <kcvolarte@gmail.com>
2025-09-02 10:37:33 +0800

    Finished quizbrain program

--- udemy_100days_angela/quizbrain/quizbrain.py
+++ udemy_100days_angela/quizbrain/quizbrain.py
@@ -3,8 +3,17 @@ class QuizBrain:
     def __init__(self, question_bank:list):
         self.question_number = 0
         self.question_bank = question_bank
+        self.score = 0
 
-    def next_question(self):
+    def still_has_questions(self):
+        return self.question_number < len(self.question_bank) 
+
+    def current_question(self):
         current_question = self.question_bank[self.question_number]
-        self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.question} True/False: ")
+        return input(f"Q:{current_question.question} True/False: ")
+
+    def check_answer(self):
+        answer = self.current_question()
+        current_question = self.question_bank[self.question_number]
+        if answer == current_question.answer:
+            self.score += 1
