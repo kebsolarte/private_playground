ba8730c4baa57c492f424b1a60d6e5e27f69897f
kebsolarte <kcvolarte@gmail.com>
2025-09-02 10:37:33 +0800

    Finished quizbrain program

--- udemy_100days_angela/quizbrain/main.py
+++ udemy_100days_angela/quizbrain/main.py
@@ -6,7 +6,15 @@ from quizbrain import QuizBrain
 question_bank =[Question(item["text"], item["answer"]) for item in question_data]
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+# Main program
+while quiz.still_has_questions():
+    quiz.check_answer()
+    print(f"The correct answer is {quiz.question_bank[quiz.question_number].answer}")
+    quiz.question_number += 1
+    print(f"Score: {quiz.score}/{quiz.question_number}")
+
+
 
 
 
