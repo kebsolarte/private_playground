fdfb38b6a2e12f4f9e3ef48f77db8cf1f407eedc
kebsolarte <kcvolarte@gmail.com>
2025-09-06 18:12:04 +0800

    Added random walk and draw shapes to turtle

--- udemy_100days_angela/turtle/main.py
+++ udemy_100days_angela/turtle/main.py
@@ -1,32 +1,57 @@
+import random
 from turtle import Turtle, Screen
 
 # Creating a Turtle object
 timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("DarkSlateGray4")
+timmy.speed("fastest")
+
+# Creating screen object
+screen = Screen()
 
 # Moving the Turtle
-def forward_right(num):
+def forward_right(num, angle):
     """Moves turtle num times forward and face right"""
     for _ in range(num):
         timmy.forward(100)
-        timmy.right(90)
+        timmy.right(angle)
 
-def dashed_line(num):
-    """Moving the turtle forward with a trailing dashed line"""
-    for _ in range(num):
-        timmy.forward(10)
-        timmy.penup()
-        timmy.forward(10)
-        timmy.pendown()
+# def dashed_line(num):
+#     """Moving the turtle forward with a trailing dashed line"""
+#     for _ in range(num):
+#         timmy.forward(10)
+#         timmy.penup()
+#         timmy.forward(10)
+#         timmy.pendown()
 
-dashed_line(10)
+def random_color():
+    """Chooses a random pen color"""
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    screen.colormode(255)
+    timmy.color(r,g,b)
 
+# def draw_shapes(list):
+#     """Move the turtle to create polygons with random pen color"""
+#     for item in list:
+#         n = item
+#         angle = 360/item
+#         random_color()
+#         forward_right(n, angle)
 
+def random_walk(steps):
+    for _ in range(steps):
+        random_color()
+        timmy.forward(30)
+        direction = random.choice([timmy.right, timmy.left])
+        direction(90)
 
+# list = [i for i in range(3,10)]
+# draw_shapes(list)
 
-
+random_walk(1000)
 
 # Displaying Turtle in a window
-screen = Screen()
 screen.exitonclick()
