453c46b08e7a099eb06e84f86bf4810a679d94d5
kebsolarte <kcvolarte@gmail.com>
2025-10-10 05:58:03 +0800

    Continued working on the snake class

--- udemy_100days_angela/snake/snake.py
+++ udemy_100days_angela/snake/snake.py
@@ -1,24 +1,39 @@
 from turtle import Turtle
 
+STARTING_POSITION = 0
+MOVE_DISTANCE =20
+
 class Snake:
+    """
+    Class to instantiate and manage the snake object in the game.
+    """
 
-    def __init__(self):
-        self.color = 'white'
-        self.shape = 'square'
-        self.length = 3
-        self.snake = []
+    def __init__(self) -> None:
+        self.segment = []
+        self.create_snake()
 
-        xcor = 0
-        for i in range(0, self.length):
-            segment = Turtle(self.shape)
-            segment.color(self.color)
+
+    def create_snake(self) -> None:
+        """
+        Creates the first three segments of the snake.
+        """
+
+        xcor = STARTING_POSITION
+        for i in range(0, 3):
+            segment = Turtle("square")
+            segment.color("white")
             segment.penup()
             segment.setx(xcor)
-            self.snake.append(segment)
+            self.segment.append(segment)
             xcor -= 20
 
 
-    def move(self):
-        for i in range(len(self.snake) - 1, 0, -1):
-            pos_prev = self.snake[i - 1].position()
-            self.snake[i].goto(pos_prev)
+    def move(self) -> None:
+        """
+        Initiates a forward cascading movement of the snake segments.
+        """
+        for i in range(len(self.segment) - 1, 0, -1):
+            pos_prev = self.segment[i - 1].position()
+            self.segment[i].goto(pos_prev)
+
+        self.segment[0].forward(MOVE_DISTANCE)
