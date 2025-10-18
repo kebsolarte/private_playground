8ac1f5e1b20cbfdf42185596b014682ab3245d44
kebsolarte <kcvolarte@gmail.com>
2025-10-19 00:22:56 +0800

    Created the food class for snake

---
+++ udemy_100days_angela/snake/food.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("circle")
+        self.color("green")
+        self.shapesize(stretch_len=1, stretch_wid=1)
+        self.speed("fastest")
+        self.refresh()
+        
+        
+    def refresh(self):
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.goto(x, y)
