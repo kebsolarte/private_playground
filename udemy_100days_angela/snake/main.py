965637c2ca4a2a8e50f9db78a6372ea29e1be2ba
kebsolarte <kcvolarte@gmail.com>
2025-10-19 00:22:56 +0800

    Created the food class for snake

--- udemy_100days_angela/snake/main.py
+++ udemy_100days_angela/snake/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.tracer(0)
 
 # Setting the snake attributes
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkeypress(fun=snake.up, key="Up")
@@ -24,6 +26,10 @@ while not game_over:
     time.sleep(0.1)
     
     snake.move()
+
+    if snake.head.distance(food) < 20:
+        print("Yummy!")
+        food.refresh()
 
 
 
