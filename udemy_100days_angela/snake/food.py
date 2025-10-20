7944d2f3d83a34466a11239cf8f7a633075b13d8
kebsolarte <kcvolarte@gmail.com>
2025-10-21 05:58:15 +0800

    Created the scoreboard and fixed some lines on main program

--- udemy_100days_angela/snake/food.py
+++ udemy_100days_angela/snake/food.py
@@ -2,8 +2,11 @@ from turtle import Turtle
 import random
 
 class Food(Turtle):
+    """
+    Class to instantiate the food of the snake.
+    """
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.penup()
         self.shape("circle")
@@ -12,8 +15,11 @@ class Food(Turtle):
         self.speed("fastest")
         self.refresh()
         
-        
-    def refresh(self):
+    
+    def refresh(self) -> None:
+        """
+        Method to refresh the location of the food.
+        """
         x = random.randint(-280, 280)
-        y = random.randint(-280, 280)
+        y = random.randint(-280, 260)   # y set to 260 to give way for the scoreboard
         self.goto(x, y)
--- udemy_100days_angela/snake/main.py
+++ udemy_100days_angela/snake/main.py
@@ -1,36 +1,45 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
+# Sets the screen attributes
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-# Setting the snake attributes
+# Instantiates the turtle objects
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
+# Sets the keybindings for the snake object
 screen.listen()
 screen.onkeypress(fun=snake.up, key="Up")
 screen.onkeypress(fun=snake.down, key="Down")
 screen.onkeypress(fun=snake.left, key="Left")
 screen.onkeypress(fun=snake.right, key="Right")
 
-
+# Main game program
 game_over = False
 while not game_over:
     screen.update()
     time.sleep(0.1)
-    
+  
     snake.move()
 
+    # Detect collision with food
     if snake.head.distance(food) < 20:
-        print("Yummy!")
         food.refresh()
+        scoreboard.update_score()
 
+    # Detect collision with wall
+    if abs(snake.head.xcor()) > 280 or abs(snake.head.ycor()) > 280:
+        game_over = True
+        scoreboard.print_game_over()
 
 
 screen.exitonclick()
