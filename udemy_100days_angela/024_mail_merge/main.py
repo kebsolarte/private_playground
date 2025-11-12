87456b8229e234df2dcdc7b40c11a97f3d700694
kebsolarte <kcvolarte@gmail.com>
2025-11-13 03:00:29 +0800

    Created mail merge program and learned about paths

---
+++ udemy_100days_angela/024_mail_merge/main.py
@@ -0,0 +1,28 @@
+# Relative paths
+LETTER_TEMPLATE_PATH = "Input/Letters/starting_letter.txt"
+GUEST_PATH = "Input/Names/invited_names.txt"
+OUTPUT_PATH = "Output/ReadyToSend"
+
+# Get list of guests
+with open(GUEST_PATH, mode='r') as file:
+    guest_list = [line.strip() for line in list(file.readlines())]
+
+# Open template
+with open(LETTER_TEMPLATE_PATH, mode="r") as file:
+    letter_template = file.read()
+
+# Main write program
+for guest in guest_list:
+
+    # Fill in template with guest name
+    filled_letter = letter_template.replace("[name]", guest)
+
+    # Create output path for each letter
+    output_file_path = f"{OUTPUT_PATH}/{guest}.txt"
+
+    # Write letter to output path
+    with open(output_file_path, mode='w') as file:
+        file.write(filled_letter)
+
+
+
