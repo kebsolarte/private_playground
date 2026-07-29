094b426fe3d3e71b3a1c677c5edf08e52033cf2c
kebsolarte <kcvolarte@gmail.com>
2026-07-29 21:59:13 +0800

    renamed files and placed some comments

--- udemy_100days_angela/032_smtp_datetime/test_email.py
+++ udemy_100days_angela/032_smtp_datetime/test_email.py
@@ -11,7 +11,7 @@ sender_email = os.getenv("WORK_GMAIL_UN")
 sender_email_pw = os.getenv("WORK_GMAIL_APP_PW")
 recipient_email = os.getenv("PERSONAL_GMAIL_UN")
 
-# Creating email message using the built in email.message module for cleaner headers
+# Optional: creating email message using the built in email.message module for cleaner headers
 msg = EmailMessage()
 msg['From'] = sender_email
 msg['To'] = recipient_email
@@ -33,4 +33,5 @@ with smtp.SMTP ("smtp.gmail.com", port=587, timeout=10) as conn:
     # When using EmailMessage objects, the .send_message should be used because it parses the obj, rather than the old .sendmail method which expects raw strings
     conn.send_message(msg=msg)
 
+# Print out confirmation
 print("Successfully sent an email!")
