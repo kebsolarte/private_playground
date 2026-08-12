c1e2b53d128342df81dbfa47312ce822ee620026
kebsolarte <kcvolarte@gmail.com>
2026-08-12 13:40:47 +0800

    Started the openweather project

---
+++ udemy_100days_angela/035_api_keys/openweather.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+
+# Load .env variables
+load_dotenv()
+
+
+# Get variable values and keys
+OW_KEY = os.getenv('OPENWEATHER_KEY')
+MY_LAT = os.getenv('MY_LAT')
+MY_LONG = os.getenv('MY_LONG')
+
+
+# Get hourly OpenWeather data
+OW_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast'
+parameters = {
+    'lat': MY_LAT,
+    'lon': MY_LONG,
+    'appid': OW_KEY,
+    'cnt': 4
+}
+
+ow_response = requests.get(url=OW_ENDPOINT, params=parameters)
+ow_response.raise_for_status()
+ow_data = ow_response.json()['list']
+ow_data_weather_ids = [item['weather'][0]['id'] for item in ow_data]
+
+
+def will_it_rain(weather_ids: list) -> bool:
+    # Original code:
+    #  for _ in weather_ids:
+    #     if _ < 700:
+    #         return True
+    # return False
+    
+    # Better code: use the lazy built-in func any() which returns true as soon as it finds something truthy
+    return any(id < 700 for id in weather_ids)
+
+print(will_it_rain(ow_data_weather_ids))
