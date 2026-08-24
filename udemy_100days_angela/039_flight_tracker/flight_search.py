9f4b17be216430df60ce860467bfaa4bdc60a6b4
kebsolarte <kcvolarte@gmail.com>
2026-08-24 21:52:13 +0800

    Created constructor and first method for FlightSearch class.

--- udemy_100days_angela/039_flight_tracker/flight_search.py
+++ udemy_100days_angela/039_flight_tracker/flight_search.py
@@ -1,3 +1,30 @@
+import requests
+from datetime import datetime   
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    """This class is responsible for talking to the Flight Search API."""
+    def __init__(self, endpoint: str, api_key: str) -> None:
+        self.base_endpoint = endpoint
+        self.api_key = api_key
+
+
+    def check_roundtrip_flights(self, origin_code: str, destination_code: str, outbound_date: datetime, return_date: datetime) -> dict:
+        """GET request to get google flights search through SerpAPI."""
+        parameters = {
+            "engine": "google_flights",
+            "departure_id": origin_code,
+            "arrival_id": destination_code,
+            "outbound_date": outbound_date,
+            "return_date": return_date,
+            "type": "1",
+            "adults": "1",
+            "currency": "USD",
+            "api_key": self.api_key,
+        }
+        response = requests.get(url=self.base_endpoint, params=parameters)
+        return response.json()
+
+
+
+    
--- udemy_100days_angela/039_flight_tracker/main.py
+++ udemy_100days_angela/039_flight_tracker/main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from pprint import pprint
 from data_manager import DataManager
+from flight_search import FlightSearch
+from datetime import datetime, timedelta
 
 
 
@@ -10,11 +12,16 @@ load_dotenv()
 SHEETY_ENDPOINT = 'https://api.sheety.co/f56d43c1df9771a798912f40b5cac00e/flightDeals/prices'
 SHEETY_TOKEN = os.getenv('SHEETY_TOKEN')
 
-sheety = DataManager(endpoint=SHEETY_ENDPOINT, token=SHEETY_TOKEN)
-sheety_data = sheety.get_saved_flights_details()
-pprint(sheety_data)
+SERPAPI_BASE_ENDPOINT = 'https://serpapi.com/search'
+SERPAPI_KEY = os.getenv('SERP_KEY')
+ORIGIN_CODE = 'MNL'
+OUTBOUND_DATE = datetime.now().date()
+RETURN_DATE = OUTBOUND_DATE + timedelta(weeks=24)
 
-sheety.update_saved_flight_lowest_price(obj_id=2, new_price=165)
-sheety_data = sheety.get_saved_flights_details()
-print()
-pprint(sheety_data)
+# sheety = DataManager(endpoint=SHEETY_ENDPOINT, token=SHEETY_TOKEN)
+# sheety_data = sheety.get_saved_flights_details()
+# pprint(sheety_data)
+
+google_flights = FlightSearch(endpoint=SERPAPI_BASE_ENDPOINT, api_key=SERPAPI_KEY)
+flights_data = google_flights.check_roundtrip_flights(origin_code=ORIGIN_CODE, destination_code='SIN', outbound_date=OUTBOUND_DATE, return_date=RETURN_DATE)
+pprint(flights_data)
