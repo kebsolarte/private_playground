2b1d6e3157fbc92a6f496316cd20ae215a6ade3b
kebsolarte <kcvolarte@gmail.com>
2026-08-31 21:55:57 +0800

    Created constructor for flight data module

--- 100days_yu/039_flight_tracker/flight_data.py
+++ 100days_yu/039_flight_tracker/flight_data.py
@@ -1,3 +1,16 @@
+import requests
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
+    """This class is responsible for structuring the flight data."""
+
+    def __init__(self, raw_json_response: dict) -> None:
+        self.price = 0
+        self.departure_country = ''
+        self.arrival_country = ''
+        self.outbound_date = ''
+        self.return_date = ''
+
+
+def find_cheapest_flight():
     pass
+
